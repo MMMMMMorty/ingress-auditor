c24131ee4b7cc73efbfba38207071591b06a76e3
Jiehong Mo <jiehong.mo@outlook.com>
2025-12-12 01:26:56 +0000

    feat: add controller tests and sample crd of ingressTLSLog

--- internal/controller/ingresstlslog_controller_test.go
+++ internal/controller/ingresstlslog_controller_test.go
@@ -18,40 +18,120 @@ package controller
 
 import (
 	"context"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	ingressauditv1alpha1 "github.com/MMMMMMorty/ingress-auditor/api/v1alpha1"
 )
 
 var _ = Describe("IngressTLSLog Controller", func() {
 	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+		const resourceNameFailure = "test-resource-failure"
 
 		ctx := context.Background()
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+		typeNamespacedNameFailure := types.NamespacedName{
+			Name:      resourceNameFailure,
+			Namespace: "default",
 		}
-		ingresstlslog := &ingressauditv1alpha1.IngressTLSLog{}
+		testIngressFailure := &networkingv1.Ingress{}
+
+		const resourceNameSuccess = "test-resource-success"
+
+		typeNamespacedNameSuccess := types.NamespacedName{
+			Name:      resourceNameSuccess,
+			Namespace: "default",
+		}
+		testIngressSuccess := &networkingv1.Ingress{}
 
 		BeforeEach(func() {
-			By("creating the custom resource for the Kind IngressTLSLog")
-			err := k8sClient.Get(ctx, typeNamespacedName, ingresstlslog)
+			By("creating the ingress resource for failure test")
+			err := k8sClient.Get(ctx, typeNamespacedNameFailure, testIngressFailure)
 			if err != nil && errors.IsNotFound(err) {
-				resource := &ingressauditv1alpha1.IngressTLSLog{
+				resource := &networkingv1.Ingress{
 					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
+						Name:      resourceNameFailure,
 						Namespace: "default",
 					},
-					// TODO(user): Specify other spec details if needed.
+					Spec: networkingv1.IngressSpec{
+						Rules: []networkingv1.IngressRule{
+							{
+								Host: "example.com",
+								IngressRuleValue: networkingv1.IngressRuleValue{
+									HTTP: &networkingv1.HTTPIngressRuleValue{
+										Paths: []networkingv1.HTTPIngressPath{
+											{
+												Path: "/",
+												PathType: func() *networkingv1.PathType {
+													pt := networkingv1.PathTypePrefix
+													return &pt
+												}(),
+												Backend: networkingv1.IngressBackend{
+													Service: &networkingv1.IngressServiceBackend{
+														Name: "nginx",
+														Port: networkingv1.ServiceBackendPort{
+															Number: 80,
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+
+			By("creating the ingress resource for successful test")
+			err = k8sClient.Get(ctx, typeNamespacedNameSuccess, testIngressSuccess)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &networkingv1.Ingress{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceNameSuccess,
+						Namespace: "default",
+						Annotations: map[string]string{
+							"nginx.ingress.kubernetes.io/permanent-redirect": "https://success.test.example.com",
+						},
+					},
+					Spec: networkingv1.IngressSpec{
+						Rules: []networkingv1.IngressRule{
+							{
+								Host: "success.example.com",
+								IngressRuleValue: networkingv1.IngressRuleValue{
+									HTTP: &networkingv1.HTTPIngressRuleValue{
+										Paths: []networkingv1.HTTPIngressPath{
+											{
+												Path: "/",
+												PathType: func() *networkingv1.PathType {
+													pt := networkingv1.PathTypePrefix
+													return &pt
+												}(),
+												Backend: networkingv1.IngressBackend{
+													Service: &networkingv1.IngressServiceBackend{
+														Name: "nginx-1",
+														Port: networkingv1.ServiceBackendPort{
+															Number: 80,
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
 				}
 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
@@ -59,27 +139,128 @@ var _ = Describe("IngressTLSLog Controller", func() {
 
 		AfterEach(func() {
 			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &ingressauditv1alpha1.IngressTLSLog{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			resource := &networkingv1.Ingress{}
+			err := k8sClient.Get(ctx, typeNamespacedNameFailure, resource)
 			Expect(err).NotTo(HaveOccurred())
 
-			By("Cleanup the specific resource instance IngressTLSLog")
+			By("Cleanup the specific resource instance ingress")
 			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
 		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
+		It("should be failed to reconcile the resource", func() {
+			By("Reconciling the ingress with HTTP but no redirect")
 			controllerReconciler := &IngressTLSLogReconciler{
-				Client:   k8sClient,
-				Scheme:   k8sClient.Scheme(),
-				Interval: 3600,
+				Client:               k8sClient,
+				Scheme:               k8sClient.Scheme(),
+				Interval:             3600,
+				IngressErrorMap:      make(map[string]error),
+				IngressUpdateTimeMap: make(map[string]time.Time),
 			}
 
 			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
+				NamespacedName: typeNamespacedNameFailure,
+			})
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("TLS is not used and redirect is not applied neither"))
+
+			By("Patching the ingress with empty TLS resource")
+			patch := client.MergeFrom(testIngressFailure.DeepCopy())
+
+			testIngressFailure.Spec.TLS[0].Hosts[0] = "example.com"
+
+			err = k8sClient.Patch(ctx, testIngressFailure, patch)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Reconciling the ingress with the missing secretName field resource")
+			_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedNameFailure,
+			})
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("the secretName does not define in ingress"))
+
+			By("Patching the ingress with the secret resource")
+			patch = client.MergeFrom(testIngressFailure.DeepCopy())
+
+			testIngressFailure.Spec.TLS[0].SecretName = "test-secret"
+
+			err = k8sClient.Patch(ctx, testIngressFailure, patch)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Reconciling the ingress with the missing secret")
+			_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedNameFailure,
+			})
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("unable to fetch secret"))
+
+			By("Creating the secret instance")
+			secret := &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-secret",
+					Namespace: "default",
+				},
+				Type: corev1.SecretTypeTLS,
+				Data: map[string][]byte{
+					"test-data": []byte("test-crt"),
+				},
+			}
+
+			err = k8sClient.Create(ctx, secret)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Reconciling the ingress with the secret without crt and key")
+			_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedNameFailure,
+			})
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("the crt or key does not exist in secret"))
+
+			By("Deleting the wrong secret and creating the correct secret")
+			err = k8sClient.Delete(ctx, secret)
+			Expect(err).NotTo(HaveOccurred())
+
+			secret.Data = map[string][]byte{
+				"tls.crt": []byte("test-crt"),
+				"tls.key": []byte("test-key"),
+			}
+
+			err = k8sClient.Create(ctx, secret)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Reconciling the TLS verification failure instance")
+			_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedNameFailure,
+			})
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("TLS verification failed"))
+
+			By("Patching the ingress without the hosts field but with secretName field")
+			patch = client.MergeFrom(testIngressFailure.DeepCopy())
+			testIngressFailure.Spec.TLS[0].Hosts = []string{}
+
+			err = k8sClient.Patch(ctx, testIngressFailure, patch)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Reconciling the hosts field does not define in the ingress")
+			_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedNameFailure,
+			})
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("the Hosts does not define in ingress"))
+		})
+
+		It("should successfully reconcile the resource", func() {
+			controllerReconciler := &IngressTLSLogReconciler{
+				Client:               k8sClient,
+				Scheme:               k8sClient.Scheme(),
+				Interval:             3600,
+				IngressErrorMap:      make(map[string]error),
+				IngressUpdateTimeMap: make(map[string]time.Time),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedNameSuccess,
 			})
 			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
 		})
 	})
 })
